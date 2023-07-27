7063e0fa80d9f45587c7730cef5e49f7ddbcf1cb
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2023-07-28 00:40:33 +0100

    ...

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -87,12 +87,24 @@ ASGI_APPLICATION = "beelearn.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django_tidb",
+            "NAME": "django",
+            "USER": "root",
+            "PASSWORD": "",
+            "HOST": "127.0.0.1",
+            "PORT": 4000,
+        },
+    }
 
 
 # Password validation
