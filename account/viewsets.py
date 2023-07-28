a15d6a0564458c935ed119ae8a0d8b12efec3113
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2023-07-28 12:59:37 +0100

    ..

--- account/viewsets.py
+++ account/viewsets.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions, permissions
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action, permission_classes
 
 
 from .models import User
@@ -17,6 +18,20 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(
             self.get_serializer(instance=request.user).data,
         )
+
+    @action(["post"], detail=False, url_path="create-user")
+    @permission_classes([permissions.AllowAny])
+    def create_user(self, request: Request):
+        device_id = request.data.get("device_id")
+
+        if not device_id:
+            raise exceptions.ParseError("device_id is required")
+
+        user, created = User.objects.get_or_create(username=device_id)
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({"key": token.key })
 
     def get_queryset(self):
         match self.request.method:
