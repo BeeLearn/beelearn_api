6a42be70a7e67630a780ce6fdf3d1c6bdcbf070d
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2023-07-28 11:04:52 +0100

    ...

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -30,6 +30,10 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
