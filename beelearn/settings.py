8b768bb4c24769678a42aa3884c967a431309692
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2023-07-28 10:57:08 +0100

    ...

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default = "django-insecure-f&4h0n2)4t1
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["beelearn.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
