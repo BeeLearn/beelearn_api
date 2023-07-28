48454340099360a96db27d62f239af5c1806963f
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2023-07-28 04:49:18 +0100

    update beelearn/asgi.py

--- beelearn/asgi.py
+++ beelearn/asgi.py
@@ -2,8 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from reward.hooks import RewardAPIHook, StreakAPIHook
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beelearn.settings")
 
 application = get_asgi_application()
@@ -14,6 +12,7 @@ from djira.consumer import Consumer
 from djira.settings import jira_settings
 
 from catalogue.hooks import CourseAPIHook, LessonAPIHook, ModuleAPIHook
+from reward.hooks import RewardAPIHook, StreakAPIHook
 
 sio = jira_settings.SOCKET_INSTANCE
 
