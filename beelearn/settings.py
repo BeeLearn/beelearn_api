4ab796ceda78485610781b0541fe0d2025a73013
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2023-08-07 15:20:42 +0100

    Bug fix

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -191,6 +191,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "account.authentication.FirebaseTokenAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 8,
     "DEFAULT_FILTER_BACKENDS": [
