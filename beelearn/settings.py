9dc36c5133d4430840b08e345bf782580c93fb97
Caleb Oguntunde <oasis.mystre@gmail.com>
2023-10-21 22:53:19 +0100

    ...

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["v1.api.usebeelearn.com"]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 BEELEARN_EXTERNAL_HOSTNAME = os.environ.get("BEELEARN_EXTERNAL_HOSTNAME")
