a046741181e8151d6932b91bf5f9846c7df6cffb
Caleb Oguntunde <oasis.mystre@gmail.com>
2023-10-21 23:18:48 +0100

    Remove print logger middleware from production

--- beelearn/middleware.py
+++ beelearn/middleware.py
@@ -1,6 +1,7 @@
-from typing import Any
-
+from logging import getLogger
 from django.http import HttpRequest
+
+logger = getLogger(__name__)
 
 
 class LoggerMiddleware:
@@ -8,9 +9,25 @@ class LoggerMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest):
-        print(request.body)
+        logger.debug(
+            "%s[%s] request body:"
+            % (
+                request.get_full_path(),
+                request.method,
+            ),
+            request.body,
+        )
+
         response = self.get_response(request)
+
         if hasattr(response, "data"):
-            print(response.data)
+            logger.debug(
+                "%s[%s] response body:"
+                % (
+                    request.get_full_path(),
+                    response.data,
+                ),
+                request.body,
+            )
 
         return response
