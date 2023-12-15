9c7f20cad60de0dd58db9ddb8aed3fc058568d6d
Caleb Oguntunde <oasis.mystre@gmail.com>
2023-12-15 05:34:14 +0100

    ...

--- beelearn/settings.py
+++ beelearn/settings.py
@@ -143,8 +143,8 @@ if DEBUG:
 else:
     DATABASES = {
         "default": dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True,
+            # conn_max_age=600,
+            # ssl_require=True,
         ),
     }
 
